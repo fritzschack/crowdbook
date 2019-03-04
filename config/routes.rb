558691a840cc769f570e57db5e6e44bae87f1750
Chris Ward <christopher.jonathon.ward@gmail.com>
2019-03-04 17:05:00 +1100

    updated views to show basic info

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:new, :create, :edit, :update]
 
-
-
+  resources :profiles, only: [:show]
+  get "/my_profile", to: "profiles#my_profile", as: "my_profile"
 
 end
