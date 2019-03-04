124412f7452caacc4f089131ba1e90aa7fa5f285
Rutger Swirski <rutgerswirski.johan@gmail.com>
2019-03-04 15:34:00 +1100

    added dependent

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :performances, dependant: :destroy
-  has_many :photos, dependant: :destroy
-  has_many :ticket_categories, dependant: :destroy
+  has_many :performances, dependent: :destroy
+  has_many :photos, dependent: :destroy
+  has_many :ticket_categories, dependent: :destroy
 
   has_many :musicians, through: :performances
   has_many :genres, through: :musicians
-  has_many :tickets, through: :ticket_categories, dependant: :destroy
+  has_many :tickets, through: :ticket_categories, dependent: :destroy
 end
--- app/models/ticket_category.rb
+++ app/models/ticket_category.rb
@@ -1,6 +1,6 @@
 class TicketCategory < ApplicationRecord
   belongs_to :event
-  has_many :tickets, dependant: :destroy
+  has_many :tickets, dependent: :destroy
 
-  has_many :orders, through: :tickets, dependant: :destroy
+  has_many :orders, through: :tickets, dependent: :destroy
 end
