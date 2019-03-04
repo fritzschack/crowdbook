a73fc216b5a6c982ae8efa8c503cf6634c04e4ea
Chris Ward <christopher.jonathon.ward@gmail.com>
2019-03-04 15:28:59 +1100

    updated models to have validations

--- app/models/ticket_category.rb
+++ app/models/ticket_category.rb
@@ -3,4 +3,8 @@ class TicketCategory < ApplicationRecord
   has_many :tickets
 
   has_many :orders, through: :tickets
+
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :description, presence: true
 end
