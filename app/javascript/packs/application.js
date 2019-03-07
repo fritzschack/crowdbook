861d86581bc3c1a50290fbe69b56d2ab8b82645a
Rutger Swirski <rutgerswirski.johan@gmail.com>
2019-03-07 17:18:43 +1100

    Map Fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,29 +1,29 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-import "bootstrap";
-import $ from 'jquery';
-import flatpickr from "flatpickr";
+// import "bootstrap";
+// import $ from 'jquery';
+// import flatpickr from "flatpickr";
 import { initMapbox } from '../plugins/init_mapbox.js';
-import { autoScrollHome } from '../components/autoScroll.js';
-import 'select2';
-import "flatpickr/dist/flatpickr.min.css";
-//maps///
+// import { autoScrollHome } from '../components/autoScroll.js';
+// import 'select2';
+// import "flatpickr/dist/flatpickr.min.css";
+// //maps///
 initMapbox();
-////////
-autoScrollHome();
+// ////////
+// autoScrollHome();
 
-flatpickr("#datepicker", {});
+// flatpickr("#datepicker", {});
 
-$(function() {
-  $('.js-new-performance-form').select2({
-    tags: true,
-    theme: "bootstrap",
-    width: '100%',
-    placeholder: 'Enter performances for your campaign'
-  });
-  $('.js-genre-selection-form').select2({
-    theme: "bootstrap",
-    width: '100%',
-    placeholder: 'Select a genre for your campaign'
-  });
-});
+// $(function() {
+//   $('.js-new-performance-form').select2({
+//     tags: true,
+//     theme: "bootstrap",
+//     width: '100%',
+//     placeholder: 'Enter performances for your campaign'
+//   });
+//   $('.js-genre-selection-form').select2({
+//     theme: "bootstrap",
+//     width: '100%',
+//     placeholder: 'Select a genre for your campaign'
+//   });
+// });
 
