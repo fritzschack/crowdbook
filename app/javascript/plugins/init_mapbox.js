683e01e5c187856f00f81ef28fbadee9464c3932
Fritz Schack <fritz.schack@gmail.com>
2019-03-08 10:33:01 +1100

    Small bs like banner, etc.

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,7 +14,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/fritzschack/cjso76anq0nit1fkbho5gr7pu'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
